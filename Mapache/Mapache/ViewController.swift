37d924fbf262d8b94575de52e5b7e1c843e065bf
Luis Felipe Salazar <luisfelipesv@users.noreply.github.com>
2018-10-15 01:10:54 -0500

    Added semantic cube and added funcs to MapacheListener

--- Mapache/Mapache/ViewController.swift
+++ Mapache/Mapache/ViewController.swift
@@ -12,6 +12,8 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
     
+    var semanticCube: JSON!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,8 +28,28 @@ class ViewController: UIViewController {
         
         }
         """
+        setup()
+        print(semanticCube["Char"]["Int"]["*"])
+        
         
         runCode(input: input)
+        
+    }
+    
+    func setup(){
+        
+        if let path = Bundle.main.path(forResource: "SemanticCube", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+                let jsonObj = try JSON(data: data)
+                semanticCube = jsonObj["semanticCube"]
+                //print(semanticCube)
+            } catch let error {
+                print("parse error: \(error.localizedDescription)")
+            }
+        } else {
+            print("Invalid filename/path.")
+        }
         
     }
     
@@ -57,6 +79,42 @@ class MapacheWalker: MapacheBaseListener {
     }
     
     override func enterCte(_ ctx: MapacheParser.CteContext) {
+        
+    }
+    
+    override func enterExp(_ ctx: MapacheParser.ExpContext) {
+        
+    }
+    
+    override func enterTipo(_ ctx: MapacheParser.TipoContext) {
+        
+    }
+    
+    override func enterCiclo(_ ctx: MapacheParser.CicloContext) {
+        
+    }
+    
+    override func enterBloque(_ ctx: MapacheParser.BloqueContext) {
+        
+    }
+    
+    override func enterFactor(_ ctx: MapacheParser.FactorContext) {
+        
+    }
+    
+    override func enterVector(_ ctx: MapacheParser.VectorContext) {
+        
+    }
+    
+    override func enterFuncion(_ ctx: MapacheParser.FuncionContext) {
+        
+    }
+    
+    override func enterLlamada(_ ctx: MapacheParser.LlamadaContext) {
+        
+    }
+    
+    override func enterMapache(_ ctx: MapacheParser.MapacheContext) {
         
     }
 }
