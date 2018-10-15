350e2381fefc191e3ca04501f6faf6a9dc5eb122
Luis Felipe Salazar <luisfelipesv@users.noreply.github.com>
2018-10-14 20:50:00 -0500

    Removed unused code

--- Mapache/Mapache/ViewController.swift
+++ Mapache/Mapache/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import Antlr4
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //let lexer = MapacheLexer(ANTLRInputStream(input))
+        
         let input = """
         mapache {
 
@@ -26,73 +25,22 @@ class ViewController: UIViewController {
         
         }
         """
+        
         do {
-        let lexer = MapacheLexer(ANTLRInputStream(input))
-        let tokens = CommonTokenStream(lexer)
-        let parser = try MapacheParser(tokens)
-        let tree = try parser.mapache()
-        
-        let walker = ParseTreeWalker()
-        let extractor = MapacheWalker.init()
-        try walker.walk(extractor, tree)
-        
-        //return tree.toStringTree(parser)
+            let lexer = MapacheLexer(ANTLRInputStream(input))
+            let tokens = CommonTokenStream(lexer)
+            let parser = try MapacheParser(tokens)
+            let tree = try parser.mapache()
+            
+            let walker = ParseTreeWalker()
+            let extractor = MapacheWalker.init()
+            try walker.walk(extractor, tree)
         } catch {
             
         }
         
-//        let inputStream = ANTLRInputStream(input)
-//        let ans = inputStream.toString()
-//        print(ans)
-        
-        
     }
     
-    
-    
-//    func parse(input: String) {
-//        let lexer = MapacheLexer(ANTLRInputStream(input))
-//        let tokens = CommonTokenStream(lexer)
-//        let parser = try MapacheParser(tokens)
-//        let tree = try parser.mapache()
-//
-//        let walker = ParseTreeWalker()
-//        let extractor = TestListener.init()
-//        try walker.walk(extractor, tree)
-//
-//        return tree.toStringTree(parser)
-//    }
-    
-//    private func walk(input: String, walker: MapacheWalker, tree: MapacheParser throws -> ParseTree) {
-//        do {
-//            let lexer =  MapacheLexer(ANTLRInputStream(input))
-//            let tokens =  CommonTokenStream(lexer)
-//            let parser = try MapacheParser(tokens)
-//
-//            let theTree = try tree(parser)
-//            let parseTreeWalker = ParseTreeWalker()
-//            try parseTreeWalker.walk(walker, theTree)
-//        } catch {
-//            print("Error \(error)")
-//        }
-//    }
-//    func x() {
-//    input = FileStream(argv[1])
-//    lexer = ChatLexer(input)
-//    stream = CommonTokenStream(lexer)
-//    parser = ChatParser(stream)
-//    tree = parser.chat()
-//
-//    output = open("output.html","w")
-//
-//    htmlChat = HtmlChatListener(output)
-//    walker = ParseTreeWalker()
-//    walker.walk(htmlChat, tree)
-//
-//    output.close()
-//    }
-
-
 }
 
 class MapacheWalker: MapacheBaseListener {
