f3cee261415e611341035229ab0b6d6702eb4fc7
Luis Felipe Salazar <luisfelipesv@users.noreply.github.com>
2018-10-18 10:48:05 -0500

    Quad and VirtualMemory

---
+++ Mapache/Mapache/Quadruple.swift
@@ -0,0 +1,23 @@
+//
+//  Quadruple.swift
+//  Mapache
+//
+//  Created by Luis Salazar on 10/18/18.
+//  Copyright © 2018 Luis Salazar. All rights reserved.
+//
+
+import Foundation
+
+class Quadruple {
+    var op: Int
+    var operandL: Int?
+    var operandR: Int?
+    var temp: Int?
+    
+    init(_ op: Int,_ opL: Int,_ opR: Int,_ temp:Int) {
+        self.op = op
+        self.operandL = opL
+        self.operandR = opR
+        self.temp = temp
+    }
+}
---
+++ Mapache/Mapache/VirtualMemory.swift
@@ -0,0 +1,18 @@
+//
+//  VirtualMemory.swift
+//  Mapache
+//
+//  Created by Luis Salazar on 10/18/18.
+//  Copyright © 2018 Luis Salazar. All rights reserved.
+//
+
+import Foundation
+
+class VirtualMemory {
+    var globalMemory = Memory()
+    var localMemory = Memory()
+    var constantsMemory = Memory()
+    
+    var quadrupleRow = [Int]()
+    
+}
