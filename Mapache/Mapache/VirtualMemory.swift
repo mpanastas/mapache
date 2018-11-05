4a60578c3e08012b2e8dfa935f6edfb60175fa32
Maria Paula <a00817285@itesm.mx>
2018-11-04 23:28:19 -0600

    virtual memory
    Added functions to state real address to virtual memory

--- Mapache/Mapache/VirtualMemory.swift
+++ Mapache/Mapache/VirtualMemory.swift
@@ -8,10 +8,20 @@
 
 import Foundation
 
+let globalStartAdress = 100000
+let localStartAdress = 110000
+let constantStartAdress = 120000
+
 class VirtualMemory {
     var globalMemory = Memory()
     var localMemory = Memory()
     var constantsMemory = Memory()
+    
+    func setValueIn(address: Int, resut: Any){
+        var realAdress = adress
+        
+        //code
+    }
     
     var quadrupleRow = [Int]()
     
